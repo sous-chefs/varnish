7c7e108f24fede14ab631374c36970110997bb92
jtimberman <joshua@opscode.com>
2012-04-09 13:43:57 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures varnish"
-version           "0.9.2"
+version           "0.9.3"
 
 recipe "varnish",      "Installs and configures varnish"
 recipe "varnish::apt_repo", "Adds the official varnish project apt repository"
