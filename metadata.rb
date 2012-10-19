c69dac17b1a2f053ee9299603e1831c164cef9e0
Ubuntu <ubuntu@ip-10-203-67-214.ec2.internal>
2012-10-19 19:28:25 +0000

    Added functionality for RHEL-type systems (CentOS, Fedora, etc) to add the YUM repository from varnish-cache.org

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,10 @@ description       "Installs and configures varnish"
 version           "0.9.3"
 
 recipe "varnish",      "Installs and configures varnish"
-recipe "varnish::apt_repo", "Adds the official varnish project apt repository"
 
-%w{ubuntu debian}.each do |os|
+recipe "varnish::apt_repo", "Adds the official varnish project apt repository"
+recipe "varnish::yum_repo", "Adds the official varnish project yum repository"
+
+%w{ubuntu debian fedora redhat rhel centos}.each do |os|
   supports os
 end
