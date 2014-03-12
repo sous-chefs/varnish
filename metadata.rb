a29734b033df15790847c602d7eccff1e649dd34
Sean OMeara <someara@opscode.com>
2014-03-12 14:31:19 -0400

    Version bump to 0.9.12

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures varnish"
-version           "0.9.11"
+version           "0.9.12"
 
 recipe "varnish",      "Installs and configures varnish"
 recipe "varnish::apt_repo", "Adds the official varnish project apt repository"
