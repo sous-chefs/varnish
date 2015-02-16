da7e920c241a1bf4f38dec37ad2ddb4a255ba5ec
Matthew Thode <mthode@mthode.org>
2015-02-16 12:46:01 -0600

    bump for dev

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Opscode, Inc.'
 maintainer_email 'cookbooks@opscode.com'
 license 'Apache 2.0'
 description 'Installs and configures varnish'
-version '1.1.0'
+version '1.1.1'
 
 recipe 'varnish', 'Installs and configures varnish'
 recipe 'varnish::repo', 'Adds the official varnish project repository'
