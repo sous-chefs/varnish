af7fce2df59a63ddaabecca0d6c4a65ab6c0935f
Matthew Thode <mthode@mthode.org>
2015-02-16 15:55:39 -0600

    bumping major instead since we dropped chef 11 support

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Opscode, Inc.'
 maintainer_email 'cookbooks@opscode.com'
 license 'Apache 2.0'
 description 'Installs and configures varnish'
-version '1.1.1'
+version '2.0.0'
 
 recipe 'varnish', 'Installs and configures varnish'
 recipe 'varnish::repo', 'Adds the official varnish project repository'
