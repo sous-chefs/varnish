0d8678c21c5d1d3b27dcda19af2580f713877ece
Martin Smith <martin@mbs3.org>
2015-06-19 16:20:57 -0500

    Update docs, Rakefile, standards
    - Update contributing and metadata author/contact info, fixes #65

---
+++ CONTRIBUTING.md
@@ -0,0 +1,4 @@
+CONTRIBUTING
+===========
+
+We generally follow the [same contributing guidelines as the chef project](https://github.com/chef/chef/blob/master/CONTRIBUTING.md).
--- recipes/repo.rb
+++ recipes/repo.rb
@@ -1,6 +1,8 @@
 # Cookbook Name:: varnish
 # Recipe:: repo
-# Author:: Patrick Connolly <patrick@myplanetdigital.com>
+#
+# Copyright 2014. Patrick Connolly <patrick@myplanetdigital.com>
+# Copyright 2015. Rackspace, US Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
