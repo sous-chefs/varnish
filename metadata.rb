fca4181eb1d9ae7267b2d360857da6552792ec13
hagihala <hagihala@hatena.ne.jp>
2016-09-28 13:20:08 +0900

    Loosen version constraints of yum and apt cookbooks

--- metadata.rb
+++ metadata.rb
@@ -12,8 +12,8 @@ recipe 'varnish::repo', 'Adds the official varnish project repository'
   supports os
 end
 
-depends 'apt', '~> 2.4'
+depends 'apt', '>= 2.4', '< 4.1'
 depends 'build-essential'
 depends 'chef-sugar'
-depends 'yum', '~> 3.0'
+depends 'yum', '>= 3.0', '< 4.1'
 depends 'yum-epel'
