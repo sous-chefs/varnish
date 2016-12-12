244481af2dfd74e5188d2046430dc22588655ad4
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2016-12-11 20:05:59 -0600

    Stub out installed_major_version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,14 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+require_relative '../libraries/helpers'
 
 RSpec.configure do |config|
   config.platform = 'ubuntu'
   config.version = '14.04'
+end
+
+def stub_resources
+  allow(VarnishCookbook::Helpers).to receive(:installed_major_version).and_return(4.0)
 end
 
 def node_resources(node)
