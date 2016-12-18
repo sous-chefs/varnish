6568e540c97e6b4231d03498257c790706201d62
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2016-12-18 17:05:26 -0600

    Bump major version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Rackspace'
 maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
 description 'Installs and configures varnish'
-version '2.5.0'
+version '3.0.0'
 
 recipe 'varnish', 'Installs and configures varnish'
 recipe 'varnish::repo', 'Adds the official varnish project repository'
