8188d8d9b3f44cfe2ed0acba6e3cc3e871a1e408
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2017-01-07 16:32:16 -0600

    Make nginx test config as minimal as possible

--- test/fixtures/cookbooks/install_varnish/recipes/full_stack.rb
+++ test/fixtures/cookbooks/install_varnish/recipes/full_stack.rb
@@ -4,20 +4,24 @@ node.override['varnish']['configure']['ncsa']['action'] = :configure
 
 package 'nginx'
 
-service 'nginx' do
+# avoid depending on init system
+execute 'nginx_restart' do
+  command 'pkill nginx; nginx'
   action :nothing
 end
 
-cookbook_file '/etc/nginx/sites-enabled/default' do
-  notifies_immediately :restart, 'service[nginx]'
-end
+public_html = '/var/www/public_html'
 
-directory '/var/www/public_html' do
+directory public_html do
   recursive true
 end
 
-file '/var/www/public_html/index.html' do
+file "#{public_html}/index.html" do
   content 'Hello World!'
+end
+
+cookbook_file '/etc/nginx/nginx.conf' do
+  notifies_immediately :run, 'execute[nginx_restart]'
 end
 
 include_recipe 'varnish::configure'
