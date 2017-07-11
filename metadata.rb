ca235cb3250c889bf28ad725e0eb930b220db92b
Steve Buzonas <steve@fancyguy.com>
2017-07-10 20:54:27 -0400

    fix license string for lint check

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'varnish'
 maintainer 'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs and configures varnish'
 version '3.2.0'
 source_url 'https://github.com/sous-chefs/varnish'
