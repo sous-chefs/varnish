c908f0178d6c5b4864180ad77ff6af65059d9d8a
Gavin Reynolds <g.reynolds@src.gla.ac.uk>
2017-09-04 12:08:12 +0100

    Update varnish cache repo to packagecloud.io
    Fix #140

--- resources/varnish_repo_debian.rb
+++ resources/varnish_repo_debian.rb
@@ -6,11 +6,13 @@ property :major_version, kind_of: Float, equal_to: [2.1, 3.0, 4.0, 4.1], default
 property :fetch_gpg_key, kind_of: [TrueClass, FalseClass], default: true
 
 action :configure do
+  # packagecloud repos omit dot from major version
+  major_version_no_dot = new_resource.major_version.to_s.tr('.', '')
   apt_repository "varnish-cache_#{new_resource.major_version}" do
-    uri "http://repo.varnish-cache.org/#{node['platform']}"
+    uri "https://packagecloud.io/varnishcache/varnish#{major_version_no_dot}/#{node['platform']}/"
     distribution node['lsb']['codename']
-    components ["varnish-#{new_resource.major_version}"]
-    key "https://repo.varnish-cache.org/#{node['platform']}/GPG-key.txt" if new_resource.fetch_gpg_key
+    components ['main']
+    key "https://packagecloud.io/varnishcache/varnish#{major_version_no_dot}/gpgkey" if new_resource.fetch_gpg_key
     deb_src true
     action :add
   end
